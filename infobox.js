e1e1826ed8b53f38e5972c3a1447729da5ed4f48
TRMSC <solidware@web.de>
2022-11-03 13:52:59 +0100

    make adjustments

--- infobox.js
+++ infobox.js
@@ -40,10 +40,11 @@ H5P.Infobox = (function ($) {
       let duration = '<div class="infobox-durationcontainer"><div class="infobox-durationstatus" style="animation: progress linear ' + progress + 's"><i class="fa fa-chevron-right infobox-icon"></i></div></div>';
       let back = (self.options.end.settings.return == 'allowed') ? '<div class="infobox-backcontainer infobox-back infobox-btn"><i class="fa fa-chevron-left infobox-icon"></i></div>' : '';
       let main = '<div class="h5p-infobox-container h5p-infobox-main">' + header + buildPage(self.options.start) + duration + '</div>';
-      let close = '<div class="h5p-infobox-container h5p-infobox-close">' + header + buildPage(self.options.end.content) + back + '</div>';
+      let close = '<div class="h5p-infobox-container h5p-infobox-close" style="display: none">' + header + buildPage(self.options.end.content) + back + '</div>';
       $container.append(main + close);
 
       // Adjust layout
+      /*
       let hm = $container.find('.h5p-infobox-main').height();
       let hc = $container.find('.h5p-infobox-close').height();
       let hf = Math.max(hm, hc) + 22 + 'px';
@@ -52,6 +53,7 @@ H5P.Infobox = (function ($) {
       $container.find('.h5p-infobox-main').height(hf);
       $container.find('.h5p-infobox-close').height(hf);
       $container.css('display', 'flex');
+      */
     };
 
     /**
@@ -64,7 +66,9 @@ H5P.Infobox = (function ($) {
       let introtext = content.introtext ? '<div class="infobox-text">' + content.introtext + '</div>' : "";
       let image = content.image && content.image.path ? '<div class="infobox-image-container"><img class="infobox-image" src="' + H5P.getPath(content.image.path, self.id) + '"></div>' : '';
       let extensiontext = content.extensiontext ? '<div class="infobox-text">' + content.extensiontext + '</div>' : '';
-      return introtext + image + extensiontext;
+      //return 
+      let outcome = '<div class="infobox-content">' + introtext + image + extensiontext + '</div>';
+      return outcome;
     };
 
     /**
@@ -182,12 +186,12 @@ H5P.Infobox = (function ($) {
     (function() {
       prepareContent();
       //$container.find('.h5p-infobox-container').css('visibility', 'hidden');
-      setTimeout(function() {
+      //setTimeout(function() {
         checkTime (progress);
         tuneRatios();
-        $container.find('.h5p-infobox-close').css('display', 'none');
+        //$container.find('.h5p-infobox-close').css('display', 'none');
         $container.find('.h5p-infobox-container').css('visibility', 'visible');
-      },300);
+      //},300);
     })();
 
   };
